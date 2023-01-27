dcf59e0c2c5660a50a75d827711f1d364493627d
Byeonggon Lee <binkoni@gonnux.com>
2023-01-27 22:59:14 +0900

    Update 1.py

--- longest-substring-without-repeating-characters/1.py
+++ longest-substring-without-repeating-characters/1.py
@@ -6,6 +6,7 @@ O(n)의 시간 복잡도로 풀어야 한다는 것이 관건
 가장 긴지는 어떻게 알까 -> 길이만 리턴하면 되므로 길이만 저장해가며 한바퀴 돌면 될듯
 루프의 정지조건은? -> 중복이 없고.. 문자열의 끝부분이 원본 문자열의 끝부분과 일치할때
 복잡도 O(n)인 이유 -> 중복체크는 해시를 써버리면 되니까 O(1), 순회의 경우 최악의경우 end는 원본 문자열 끝까지 n번, start 또한 원본 문자열 끝까지 n번 가기 떄문에 2n이므로 O(n)
+루프 이전 혹은 이후에 한번 더 로직을 실행시켜야 할 것 같다. 끝부분에서 에러가 남
 '''
 '''
 pwpwkew -> pwke
@@ -20,10 +21,42 @@ pwpw[kew]
 '''
 
 class Solution:
-    def doesRepeat(chars, start, end):
+    def doesRepeat(self, counts):
+        for char in counts:
+            if counts[char] > 1:
+                return True
+        return False
+        
+    def increaseCount(self, counts, char):
+        if char not in counts:
+            counts[char] = 1
+        else:
+            counts[char] += 1
+    
+    def decreaseCount(self, counts, char):
+        counts[char] -= 1
+        
 
     def lengthOfLongestSubstring(self, s: str) -> int:
-        chars = list(s)
+        if len(s) == 0:
+            return 0
         counts = {}
-        start = end = 0 
-        while end < len(chars) - 1:
+        start = 0
+        end = 1
+        max_len = 0
+
+        self.increaseCount(counts, s[end - 1])
+        while end <= len(s):
+            print(s[start:end], s)
+            print(counts)
+            print('before test')
+            if self.doesRepeat(counts):
+                print('does repeat')
+                self.decreaseCount(counts, s[start])
+                start += 1
+            else:
+                print('does not repeat')
+                max_len = max(max_len, len(s[start:end]))
+                end += 1
+                self.increaseCount(counts, s[end - 1])
+        return max_len
