e5fd3e28327dbe5b1f24218a5fff891277e76d99
Byeonggon Lee <binkoni@gonnux.com>
2023-02-13 21:32:57 +0900

    Update 1.py

--- median-of-two-sorted-arrays/1.py
+++ median-of-two-sorted-arrays/1.py
@@ -1,3 +1,5 @@
+'''
+# O(n + m) Solution
 import math
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
@@ -23,3 +25,36 @@ class Solution:
             k +=1
 
         return (nums3[math.floor((n + m) / 2)] + nums3[math.floor((n + m - 1) / 2)]) / 2
+'''
+# https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/2511/intuitive-python-o-log-m-n-solution-by-kth-smallest-in-the-two-sorted-arrays-252ms/?orderBy=most_votes&languageTags=python
+
+class Solution:
+    def findMedianSortedArrays(self, A, B):
+        l = len(A) + len(B)
+        if l % 2 == 1:
+            return self.kth(A, B, l // 2)
+        else:
+            return (self.kth(A, B, l // 2) + self.kth(A, B, l // 2 - 1)) / 2.   
+    
+    def kth(self, a, b, k):
+        if not a:
+            return b[k]
+        if not b:
+            return a[k]
+        ia, ib = len(a) // 2 , len(b) // 2
+        ma, mb = a[ia], b[ib]
+    
+        # when k is bigger than the sum of a and b's median indices 
+        if ia + ib < k:
+            # if a's median is bigger than b's, b's first half doesn't include k
+            if ma > mb:
+                return self.kth(a, b[ib + 1:], k - ib - 1)
+            else:
+                return self.kth(a[ia + 1:], b, k - ia - 1)
+        # when k is smaller than the sum of a and b's indices
+        else:
+            # if a's median is bigger than b's, a's second half doesn't include k
+            if ma > mb:
+                return self.kth(a[:ia], b, k)
+            else:
+                return self.kth(a, b[:ib], k)
